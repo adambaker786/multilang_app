9c84feec51bd5c689dc9fccb939c21aaa3a680b9
adambaker786 <adambeno@mail.ru>
2023-10-21 12:57:33 +0200

    working on hash

--- app.js
+++ app.js
@@ -4,4 +4,24 @@ const allLang = ['en', 'ru', 'ua'];
 
 document.querySelectorAll('.left-panel img').forEach(item => item.onmouseenter = (event) => maxImg.src = event.target.src);
 
-// select.addEventListener('change', changeURLLanguage);
+select.addEventListener('change', changeURLLanguage);
+
+// rederect on URL with choused language
+function changeURLLanguage() {
+  let lang = select.value;
+  location.href = window.location.pathname + '#' + lang;
+  location.reload();
+}
+
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substring(1);
+  console.log(hash);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + '#en';
+    location.reload();
+  }
+  select.value = hash;
+}
+
+changeLanguage();
